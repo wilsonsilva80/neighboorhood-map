e8ca97dbd1a15fb6d3ee31f461e5c245b9b6adf6
Wilson Silva <wilsonsilva80@gmail.com>
2018-08-05 03:43:06 +0100

    readme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import MapContainer from './Components/MapContainer'
-
 import './App.css';
 
 class App extends Component {
-
-    // function initMap() {
-    //
-    // }
 
   render() {
     return (
         <React.Fragment>
             <MapContainer />
-
         </React.Fragment>
     );
   }
